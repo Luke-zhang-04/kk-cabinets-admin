a276f5c5e3167642327b4a26155f73b7e6e78283
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2020-04-08 09:47:37 -0400

    added editor role

--- functions/index.js
+++ functions/index.js
@@ -36,10 +36,31 @@ exports.addAdminRole = functions.https.onCall((data, context) => {
     //get user and add custom claim (admin)
     return admin.auth().getUserByEmail(data.email).then(user => {
         database.ref("admins/" + user.uid).set({"email": user.email})
-        return admin.auth().setCustomUserClaims(user.uid, {admin: true})
+        database.ref("editors/" + user.uid).remove()
+        return admin.auth().setCustomUserClaims(user.uid, {admin: true, editor: false})
     }).then(() => {
         return {
             message: `Success! ${data.email} has been made an admin`
+        }
+    }).catch(err => {
+        return err
+    })
+})
+
+exports.addEditorRole = functions.https.onCall((data, context) => {
+    //check for damin creds
+    if (!context.auth.token.admin) {
+        return {message: `ERROR! Only admins can add other admins\nThought you could get around security, ${data.email}?`}
+    }
+
+    //get user and add custon claim (editor)
+    return admin.auth().getUserByEmail(data.email).then(user => {
+        database.ref("editors/" + user.uid).set({"email": user.email})
+        database.ref("admins/" + user.uid).remove()
+        return admin.auth().setCustomUserClaims(user.uid, {admin: false, editor: true})
+    }).then(() => {
+        return {
+            message: `Success! ${data.email} has been made an editor`
         }
     }).catch(err => {
         return err
