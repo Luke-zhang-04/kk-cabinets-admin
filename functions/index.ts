da6174802045656c52d9853f0c2fd81e381ed5f8
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2020-04-07 22:45:44 -0400

    typescript

---
+++ functions/index.ts
@@ -0,0 +1,47 @@
+/*
+	KK Cabinets
+    Copyright (C) 2020 Luke Zhang, Ethan Lim
+    
+    https://github.com/Luke-zhang-04
+    https://github.com/ethanlim04
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+
+admin.initializeApp({
+    databaseURL: "https://kk-cabinets.firebaseio.com/"
+})
+let database = admin.database()
+
+
+exports.addAdminRole = functions.https.onCall((data, context) => {
+    //check for admin creds
+    if (!context.auth.token.admin) {
+        return {message: `ERROR! Only admins can add other admins\nThought you could get around security, ${data.email}?`}
+    }
+
+    //get user and add custom claim (admin)
+    return admin.auth().getUserByEmail(data.email).then(user => {
+        database.ref("admins/" + user.uid).set({"email": user.email})
+        return admin.auth().setCustomUserClaims(user.uid, {admin: true})
+    }).then(() => {
+        return {
+            message: `Success! ${data.email} has been made an admin`
+        }
+    }).catch(err => {
+        return err
+    })
+})
